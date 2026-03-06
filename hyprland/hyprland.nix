9fcb93a3246abf6fda07db05f7b3c7a5dac2fac3
MichaelFisher1997 <contact@michaelfisher.tech>
2026-03-06 20:44:07 +0000

    refactor: manage desktop themes in home-manager

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -46,7 +46,48 @@
   services.dunst.settings = {
     global = {
       font = "Noto Sans 10";
+      frame_color = "#4c566a";
+      separator_color = "#3b4252";
+      corner_radius = 10;
+      gap_size = 8;
+      padding = 12;
+      horizontal_padding = 12;
+      background = "#1f2329";
+      foreground = "#eceff4";
+      highlight = "#88c0d0";
+      timeout = 8;
     };
+
+    urgency_low = {
+      background = "#1f2329";
+      foreground = "#d8dee9";
+      frame_color = "#4c566a";
+      timeout = 4;
+    };
+
+    urgency_normal = {
+      background = "#1f2329";
+      foreground = "#eceff4";
+      frame_color = "#88c0d0";
+      timeout = 8;
+    };
+
+    urgency_critical = {
+      background = "#2b1f23";
+      foreground = "#eceff4";
+      frame_color = "#bf616a";
+      timeout = 0;
+    };
+  };
+
+  xdg.configFile."waybar/style.css" = {
+    source = ../waybar/style.css;
+    force = true;
+  };
+
+  xdg.configFile."rofi/config.rasi" = {
+    source = ../rofi/config.rasi;
+    force = true;
   };
 
   # Start waybar via systemd for reliability
@@ -59,6 +100,7 @@
     Service = {
       Type = "simple";
       ExecStart = "${pkgs.waybar}/bin/waybar";
+      ExecReload = "${pkgs.procps}/bin/pkill -x waybar; ${pkgs.waybar}/bin/waybar";
       Restart = "on-failure";
       RestartSec = 3;
     };
--- modules/gtk.nix
+++ modules/gtk.nix
@@ -1,30 +1,62 @@
-{ ... }:
-{
-  xdg.configFile."gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-application-prefer-dark-theme=true
-    gtk-cursor-theme-name=breeze_cursors
-    gtk-cursor-theme-size=24
-    gtk-decoration-layout=icon:minimize,maximize,close
-    gtk-enable-animations=true
-    gtk-font-name=Noto Sans 10
-    gtk-icon-theme-name=breeze-dark
-    gtk-primary-button-warps-slider=true
-    gtk-sound-theme-name=ocean
-    gtk-xft-dpi=122880
-  '';
+{ pkgs, ... }:
 
-  xdg.configFile."gtk-4.0/settings.ini".text = ''
-    [Settings]
-    gtk-application-prefer-dark-theme=true
-    gtk-cursor-theme-name=breeze_cursors
-    gtk-cursor-theme-size=24
-    gtk-decoration-layout=icon:minimize,maximize,close
-    gtk-enable-animations=true
-    gtk-font-name=Noto Sans 10
-    gtk-icon-theme-name=breeze-dark
-    gtk-primary-button-warps-slider=true
-    gtk-sound-theme-name=ocean
-    gtk-xft-dpi=122880
-  '';
+let
+  gtkThemeName = "Breeze-Dark";
+  iconThemeName = "breeze-dark";
+  cursorThemeName = "Bibata-Modern-Ice";
+in
+{
+  gtk = {
+    enable = true;
+
+    font = {
+      name = "Noto Sans";
+      size = 10;
+    };
+
+    theme = {
+      name = gtkThemeName;
+      package = pkgs.kdePackages.breeze-gtk;
+    };
+
+    iconTheme = {
+      name = iconThemeName;
+      package = pkgs.kdePackages.breeze-icons;
+    };
+
+    cursorTheme = {
+      name = cursorThemeName;
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-decoration-layout = "icon:minimize,maximize,close";
+      gtk-enable-animations = true;
+      gtk-primary-button-warps-slider = true;
+      gtk-xft-dpi = 122880;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-decoration-layout = "icon:minimize,maximize,close";
+      gtk-enable-animations = true;
+      gtk-primary-button-warps-slider = true;
+      gtk-xft-dpi = 122880;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      cursor-size = 24;
+      cursor-theme = cursorThemeName;
+      document-font-name = "Noto Sans 10";
+      font-name = "Noto Sans 10";
+      gtk-theme = gtkThemeName;
+      icon-theme = iconThemeName;
+      monospace-font-name = "Iosevka 10";
+    };
+  };
 }
