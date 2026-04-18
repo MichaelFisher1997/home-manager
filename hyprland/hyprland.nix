e6fa852ff37106560075c1a237d85a5d8b84fa00
MichaelFisher1997 <contact@michaelfisher.tech>
2026-04-18 04:01:02 +0100

    feat: make Waybar laptop-aware

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -95,7 +95,15 @@ in {
   };
 
   xdg.configFile."waybar/config.jsonc" = {
-    source = ../waybar/config.jsonc;
+    text = lib.replaceStrings
+      [ "@MODULES_LEFT@" ]
+      [
+        (if isLaptop then
+          ''["custom/launcher", "hyprland/workspaces", "backlight", "battery", "custom/playerctl", "custom/playerlabel"]''
+        else
+          ''["custom/launcher", "hyprland/workspaces", "custom/playerctl", "custom/playerlabel"]'')
+      ]
+      (builtins.readFile ../waybar/config.jsonc);
     force = true;
   };
 
